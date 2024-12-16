5cba0b3e4655a06c6de484db2ea9cbfae17bfa44
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-12-16 05:55:15 -0600

    Test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import sys
 
-
 def main():
     valid_commands = []
     while True:
-      sys.stdout.write("$ ")
-      sys.stdout.flush()
-      user_command = input()
-      if user_command not in valid_commands:
-        print(f"${user_command}: command not found")
-#main check
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        user_command = input()
+        if user_command not in valid_commands:
+            print(f"{user_command}: command not found")
+
+# main check
 if __name__ == "__main__":
     main()
