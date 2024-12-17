c6dd9277d23ab4e3f89dedf80818d78edb0a07ad
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-12-16 20:47:24 -0600

    testing

--- app/main.py
+++ app/main.py
@@ -12,12 +12,10 @@ def main():
 
         # Echo 'exit 0' and exit cleanly
         if user_command == "exit 0":
-            print(user_command)  # Echo the exit command exactly
             sys.exit(0)  # Terminate with exit code 0
         
         # Handle invalid commands
         if user_command not in valid_commands:
-            print(user_command)  # Echo the user command
             print(f"{user_command}: command not found")
 
 if __name__ == "__main__":
