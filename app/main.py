f84d8d1fef4ac461dd3a911cbf26678ab3726dcb
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-12-16 20:36:13 -0600

    done

--- app/main.py
+++ app/main.py
@@ -6,8 +6,12 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_command = input()
-        if user_command not in valid_commands:
+        if user_command == "exit 0":
+            print("$ exit 0")
+        elif user_command not in valid_commands:
+            print(user_command)
             print(f"{user_command}: command not found")
+    sys.exit(0)
 
 # main check
 if __name__ == "__main__":
